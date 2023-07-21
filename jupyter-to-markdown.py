d4e1aa5151ee8976fbef5c98a1bba4b981a93037
kalyanoliveira <kalyan.c.o.010@gmail.com>
2023-07-21 12:24:31 -0300

    OUTPUT in jupyter-to-md parsing

--- jupyter-to-markdown.py
+++ jupyter-to-markdown.py
@@ -1,9 +1,10 @@
+import re
 import os
 import nbformat
 from nbconvert import MarkdownExporter
 
-jupyter_file_dir = os.path.join("filtragem-colaborativa")
-jupyter_file_name = "surprise-CF"
+jupyter_file_dir = os.path.join("filtragem-colaborativa", "vizinhanca")
+jupyter_file_name = "surprise-FC-vizinhanca"
 jupyter_file_path = os.path.join(jupyter_file_dir, jupyter_file_name + ".ipynb")
 
 with open(jupyter_file_path, 'r') as f:
@@ -12,7 +13,25 @@ with open(jupyter_file_path, 'r') as f:
 markdown_exporter = MarkdownExporter()
 markdown_output, _ = markdown_exporter.from_notebook_node(notebook)
 
-md_file_path = os.path.join(jupyter_file_dir, "to-markdown-" + jupyter_file_name + ".md")
+md_file_path = os.path.join(jupyter_file_dir, "to-md-" + jupyter_file_name + ".md")
 
 with open(md_file_path, 'w') as f:
     f.write(markdown_output)
+
+with open(md_file_path, 'r') as f:
+    lines = f.readlines()
+
+    for index in range(len(lines) - 2):
+        content = lines[index] + lines[index + 1] + lines[index + 2]
+        p = r'```\n\n    +.'
+        if re.search(pattern=p, string=content):
+            lines[index + 1] = "\n    OUTPUT\n"
+
+    for index in range(len(lines) - 5):
+        content = lines[index] + lines[index + 1] + lines[index + 2] + lines[index + 3] + lines[index + 4] + lines[index + 5]
+        p = r'```\n\n\n\n\n    +.'
+        if re.search(pattern=p, string=content):
+            lines[index + 4] = "\n    OUTPUT\n"
+
+with open(md_file_path, "w") as f:
+    f.writelines(lines)
