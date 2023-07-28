e10c9552d60a3363a87f8e9febe27da7a150fd23
kalyanoliveira <kalyan.c.o.010@gmail.com>
2023-07-28 10:42:18 -0300

    Add loop to ipynb-to-md script

--- jupyter-to-markdown.py
+++ jupyter-to-markdown.py
@@ -3,35 +3,40 @@ import os
 import nbformat
 from nbconvert import MarkdownExporter
 
-jupyter_file_dir = os.path.join("filtragem-baseada-conteudo")
-jupyter_file_name = "sklearn-FConteudo-TFIDF"
-jupyter_file_path = os.path.join(jupyter_file_dir, jupyter_file_name + ".ipynb")
+jupyter_files_dir = "jupyter-nbs"
+markdown_files_dir = "markdowns"
 
-with open(jupyter_file_path, 'r') as f:
-    notebook = nbformat.read(f, as_version=4)
+for jupyter_file in os.listdir(jupyter_files_dir):
 
-markdown_exporter = MarkdownExporter()
-markdown_output, _ = markdown_exporter.from_notebook_node(notebook)
+    jupyter_file_name = jupyter_file.split(".")[0]
 
-md_file_path = os.path.join(jupyter_file_dir, "to-md-" + jupyter_file_name + ".md")
+    jupyter_file_path = os.path.join(jupyter_files_dir, jupyter_file_name + ".ipynb")
 
-with open(md_file_path, 'w') as f:
-    f.write(markdown_output)
+    with open(jupyter_file_path, 'r') as f:
+        notebook = nbformat.read(f, as_version=4)
 
-with open(md_file_path, 'r') as f:
-    lines = f.readlines()
+    markdown_exporter = MarkdownExporter()
+    markdown_output, _ = markdown_exporter.from_notebook_node(notebook)
 
-    for index in range(len(lines) - 2):
-        content = lines[index] + lines[index + 1] + lines[index + 2]
-        p = r'```\n\n    +.'
-        if re.search(pattern=p, string=content):
-            lines[index + 1] = "\n    OUTPUT\n"
+    md_file_path = os.path.join(markdown_files_dir, "to-md-" + jupyter_file_name + ".md")
 
-    for index in range(len(lines) - 5):
-        content = lines[index] + lines[index + 1] + lines[index + 2] + lines[index + 3] + lines[index + 4] + lines[index + 5]
-        p = r'```\n\n\n\n\n    +.'
-        if re.search(pattern=p, string=content):
-            lines[index + 4] = "\n    OUTPUT\n"
+    with open(md_file_path, 'w') as f:
+        f.write(markdown_output)
 
-with open(md_file_path, "w") as f:
-    f.writelines(lines)
+    # with open(md_file_path, 'r') as f:
+    #     lines = f.readlines()
+
+    #     for index in range(len(lines) - 2):
+    #         content = lines[index] + lines[index + 1] + lines[index + 2]
+    #         p = r'```\n\n    +.'
+    #         if re.search(pattern=p, string=content):
+    #             lines[index + 1] = "\n    OUTPUT\n"
+
+    #     for index in range(len(lines) - 5):
+    #         content = lines[index] + lines[index + 1] + lines[index + 2] + lines[index + 3] + lines[index + 4] + lines[index + 5]
+    #         p = r'```\n\n\n\n\n    +.'
+    #         if re.search(pattern=p, string=content):
+    #             lines[index + 4] = "\n    OUTPUT\n"
+
+    # with open(md_file_path, "w") as f:
+    #     f.writelines(lines)
